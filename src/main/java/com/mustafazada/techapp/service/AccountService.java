ff0460b9c47c3a673316242c7d02de1171997f97
Ilkin <ilkinmustafazadeh@gmail.com>
2023-04-04 21:24:21 +0400

    add new transfer amount service and fix ApplicationConfig class for Security

--- src/main/java/com/mustafazada/techapp/service/AccountService.java
+++ src/main/java/com/mustafazada/techapp/service/AccountService.java
@@ -1,15 +1,19 @@
 package com.mustafazada.techapp.service;
 
-import com.mustafazada.techapp.dto.response.AccountResponseDTOList;
-import com.mustafazada.techapp.dto.response.CommonResponseDTO;
-import com.mustafazada.techapp.dto.response.Status;
-import com.mustafazada.techapp.dto.response.StatusCode;
+import com.mustafazada.techapp.dto.request.AccountToAccountRequestDTO;
+import com.mustafazada.techapp.dto.response.*;
+import com.mustafazada.techapp.entity.Account;
 import com.mustafazada.techapp.entity.TechUser;
+import com.mustafazada.techapp.exception.InvalidDTO;
+import com.mustafazada.techapp.repository.AccountRepository;
 import com.mustafazada.techapp.repository.UserRepository;
 import com.mustafazada.techapp.util.CurrentUser;
+import com.mustafazada.techapp.util.DTOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @Service
@@ -20,6 +24,12 @@ public class AccountService {
     @Autowired
     private CurrentUser currentUser;
 
+    @Autowired
+    private DTOUtil dtoUtil;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
     public CommonResponseDTO<?> getAccount() {
         Optional<TechUser> user = userRepository.findByPin(currentUser.getCurrentUser().getUsername());
 
@@ -29,5 +39,83 @@ public class AccountService {
                         .statusCode(StatusCode.SUCCESS)
                         .message("Accounts Successfully fetched!")
                         .build()).build();
+    }
+
+    @Transactional
+    public CommonResponseDTO<?> account2account(AccountToAccountRequestDTO accountToAccountRequestDTO) {
+        dtoUtil.isValid(accountToAccountRequestDTO);
+
+        if (accountToAccountRequestDTO.getAmount().compareTo(BigDecimal.ZERO) <= 0) {
+            throw InvalidDTO.builder()
+                    .responseDTO(CommonResponseDTO.builder().status(Status.builder()
+                            .statusCode(StatusCode.INVALID_DTO)
+                            .message("Amount is not correct")
+                            .build()).build()).build();
+        } else if (accountToAccountRequestDTO.getDebitAccount().equals(accountToAccountRequestDTO.getCreditAccount())) {
+            throw InvalidDTO.builder()
+                    .responseDTO(CommonResponseDTO.builder()
+                            .status(Status.builder()
+                                    .statusCode(StatusCode.INVALID_DTO)
+                                    .build()).build()).build();
+        }
+
+        Optional<Account> byDebitAccountNo = accountRepository.findByAccountNo(accountToAccountRequestDTO.getDebitAccount());
+
+        Account debitAccount;
+        Account creditAccount;
+        if (byDebitAccountNo.isPresent()) {
+            debitAccount = byDebitAccountNo.get();
+            if (!debitAccount.getIsActive()) {
+                throw InvalidDTO.builder()
+                        .responseDTO(CommonResponseDTO.builder()
+                                .status(Status.builder()
+                                        .statusCode(StatusCode.INVALID_DTO)
+                                        .message("Debit Account is not active")
+                                        .build()).build()).build();
+            }
+            if (debitAccount.getBalance().compareTo(accountToAccountRequestDTO.getAmount()) <= 0) {
+                throw InvalidDTO.builder()
+                        .responseDTO(CommonResponseDTO.builder()
+                                .status(Status.builder()
+                                        .statusCode(StatusCode.INVALID_DTO)
+                                        .message("Balance is not enough")
+                                        .build()).build()).build();
+            }
+            Optional<Account> byCreditAccountNo = accountRepository.findByAccountNo(accountToAccountRequestDTO.getCreditAccount());
+            if (byCreditAccountNo.isPresent()) {
+                creditAccount = byCreditAccountNo.get();
+                if (!creditAccount.getIsActive()) {
+                    throw InvalidDTO.builder()
+                            .responseDTO(CommonResponseDTO.builder()
+                                    .status(Status.builder()
+                                            .statusCode(StatusCode.INVALID_DTO)
+                                            .message("Credit Account is not active")
+                                            .build()).build()).build();
+                }
+            } else {
+                throw InvalidDTO.builder().responseDTO(CommonResponseDTO.builder().status(Status.builder()
+                        .statusCode(StatusCode.INVALID_DTO)
+                        .message("Credit account is not present")
+                        .build()).build()).build();
+            }
+        } else {
+            throw InvalidDTO.builder().responseDTO(CommonResponseDTO.builder().status(Status.builder()
+                    .statusCode(StatusCode.INVALID_DTO)
+                    .message("Debit account is not present")
+                    .build()).build()).build();
+        }
+
+        debitAccount.setBalance(debitAccount.getBalance().subtract(accountToAccountRequestDTO.getAmount()));
+        creditAccount.setBalance(creditAccount.getBalance().add(accountToAccountRequestDTO.getAmount()));
+
+        return CommonResponseDTO.builder().status(Status.builder()
+                .statusCode(StatusCode.SUCCESS)
+                .message("Transfer completed successfully")
+                .build()).data(AccountResponseDTO.builder()
+                .balance(debitAccount.getBalance())
+                .currency(debitAccount.getCurrency())
+                .isActive(debitAccount.getIsActive())
+                .accountNo(debitAccount.getAccountNo())
+                .build()).build();
     }
 }
