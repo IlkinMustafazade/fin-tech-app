6a33caa5e110ecb8bdbf0446f64f5d26e00c8439
Ilkin <ilkinmustafazadeh@gmail.com>
2024-04-25 21:39:01 +0400

    add getAccounts Service and JWT token

--- src/main/java/com/mustafazada/techapp/service/UserLoginService.java
+++ src/main/java/com/mustafazada/techapp/service/UserLoginService.java
@@ -1,6 +1,8 @@
 package com.mustafazada.techapp.service;
 
+import com.mustafazada.techapp.config.security.JWTUtil;
 import com.mustafazada.techapp.dto.request.AuthenticationRequestDTO;
+import com.mustafazada.techapp.dto.response.AuthenticationResponseDTO;
 import com.mustafazada.techapp.dto.response.CommonResponseDTO;
 import com.mustafazada.techapp.dto.response.Status;
 import com.mustafazada.techapp.dto.response.StatusCode;
@@ -11,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,7 +22,9 @@ import org.springframework.stereotype.Service;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserLoginService {
     AuthenticationManager authenticationManager;
+    UserDetailsService userDetailsService;
     DTOCheckUtil dtoCheckUtil;
+    JWTUtil jwtUtil;
 
     public CommonResponseDTO<?> loginUser(AuthenticationRequestDTO authenticationRequestDTO) {
         dtoCheckUtil.isValid(authenticationRequestDTO);
@@ -27,20 +33,22 @@ public class UserLoginService {
                     authenticationRequestDTO.getPin(),
                     authenticationRequestDTO.getPassword()
             ));
-        }catch (Exception e){
+        } catch (Exception e) {
             throw BadInputCredentialsException.builder().responseDTO(CommonResponseDTO.builder()
                     .status(Status.builder()
                             .statusCode(StatusCode.BAD_CREDENTIALS)
                             .message("The Credential is incorrect. Pin: "
                                     + authenticationRequestDTO.getPin() + " or Password: "
                                     + authenticationRequestDTO.getPassword() + " is wrong")
-                    .build()).build()).build();
+                            .build()).build()).build();
         }
 
-
+        UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequestDTO.getPin());
         return CommonResponseDTO.builder().status(Status.builder()
-                        .statusCode(StatusCode.SUCCESS)
-                        .message("Welcome to FIN_TECH application")
-                .build()).data(authenticationRequestDTO).build();
+                .statusCode(StatusCode.SUCCESS)
+                .message("Welcome to FIN_TECH application. Token was generated SUCCESSFULLY.")
+                .build()).data(AuthenticationResponseDTO.builder()
+                .token(jwtUtil.createToken(userDetails))
+                .build()).build();
     }
 }
