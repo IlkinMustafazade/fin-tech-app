5555e819bf2a5a8a610fcf8a9c1ca221c1008aa0
Ilkin <ilkinmustafazadeh@gmail.com>
2025-05-04 20:21:42 +0400

    register service added

---
+++ src/main/java/com/mustafazada/techapp/exception/GlobalExceptionHandling.java
@@ -0,0 +1,40 @@
+package com.mustafazada.techapp.exception;
+
+import com.mustafazada.techapp.dto.response.CommonResponseDTO;
+import com.mustafazada.techapp.dto.response.Status;
+import com.mustafazada.techapp.dto.response.StatusCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandling {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<?> internalError(Exception e) {
+        e.printStackTrace();
+        return new ResponseEntity<>
+                (
+                        CommonResponseDTO.builder().status(Status.builder()
+                                .statusCode(StatusCode.INTERNAL_ERROR)
+                                .message("Internal Error")
+                                .build()).build(), HttpStatus.INTERNAL_SERVER_ERROR
+                );
+    }
+
+    @ExceptionHandler(value = InvalidDTOException.class)
+    public ResponseEntity<?> invalidDTO(InvalidDTOException invalidDTOException) {
+        return new ResponseEntity<>(invalidDTOException.getResponseDTO(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = UserAlreadyExistException.class)
+    public ResponseEntity<?> userExist(UserAlreadyExistException userAlreadyExistException) {
+        return new ResponseEntity<>(userAlreadyExistException.getResponseDTO(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(value = AccountAlreadyExistException.class)
+    public ResponseEntity<?> accountExist(AccountAlreadyExistException accountAlreadyExistException) {
+        return new ResponseEntity<>(accountAlreadyExistException.getResponseDTO(), HttpStatus.CONFLICT);
+    }
+}
